b66af7a00ab3105cee62f223b95d1a1b6518f0f1
SvetlDI <svetlana24.dubrova@gmail.com>
2021-12-22 17:16:52 +0500

    6.4. Слово начинается с ... [#41585 #242630

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -6,7 +6,6 @@ public class EndsWith {
         for (int i = 0; i < post.length; i++) {
             if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
                 result = false;
-                break;
             }
 
         }
